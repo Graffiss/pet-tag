791215ab6d81ad4883e7ed779a50294f42036c4c
Graffiss <graffis@gmail.com>
2024-04-21 18:52:12 +0200

    Added unknown-endpoint middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import petRouter from "./routes/pet/router";
 import userRouter from "./routes/user/router";
+import { unknownEndpoint } from "./middlewares/unknown-endpoint";
 
 const app = express();
 const router = Router();
@@ -13,5 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router).use("/api/user", userRouter).use("/api", petRouter);
+
+app.use(unknownEndpoint);
 
 export default app;
